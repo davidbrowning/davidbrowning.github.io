d04a9a5510a1e384eafd0077ce3f8c59fd44d6f8
davidbrowning <dave.browning21@aggiemail.usu.edu>
2018-08-05 10:06:03 -0600

    original posh script

---
+++ wordextractor1.0.1.ps1
@@ -0,0 +1,58 @@
+﻿
+function done(){
+  $word.ActiveDocument.Close();
+  $word.Quit();
+  write-host "Done"
+  exit
+  end
+}
+
+cd "C:\Users\Dave\Desktop\Grandma and Grandpa Browning\"
+
+
+foreach ($file in $(get-childitem *.doc*)) # had to also change to .doc in retrospect, should have used *.doc*
+{
+ Write-Host $file; 
+
+
+$word = New-Object -comobject Word.Application
+$word.Visible = $False 
+$doc = $word.Documents.Open("$file") 
+$title = $word.ActiveDocument.Name.Split( "\.",2).GetValue(0) + ".html"
+Write-Host $title
+#done
+
+$path = "C:\Users\Dave\Desktop\Grandma and Grandpa Browning\htmlout\";
+mkdir $path
+$path += $title
+#$path = $title;
+$doc.SaveAs([ref]$path,8);
+#done
+<#
+$a =0;
+foreach ($p in $paras)
+{
+  #Write-Host $a
+  
+  try{get-date($p.Range.Text) -ErrorAction SilentlyContinue;}catch{}
+
+  if([string]::IsNullOrWhiteSpace($p.Range.Text))
+  {
+  }  
+  elseif($?)
+  {
+     write-host "DATE: " $p.Range.Text;
+  }
+  else
+  {
+     write-host "CONTENT: " $p.Range.Text;
+  }
+    
+  $a++;
+}
+#$doc.close();
+#>
+$word.ActiveDocument.Close();
+}
+
+done
