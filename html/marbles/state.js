fa3ccc3096ef96fe3853551c76da6df5cfdfcd58
Dave Browning <dmbrowning21@gmail.com>
2025-06-15 20:15:08 -0400

    1 shot marbles rewrite

--- html/marbles/state.js
+++ html/marbles/state.js
@@ -205,7 +205,7 @@ function AboutState(name) {
 	var scene = new Scene(canvas.width, canvas.height),
 		ctx = scene.getContext();
 
-	var text = "Tic-tac-toe (or Noughts and crosses, Xs and Os) is a game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three respective marks in a horizontal, vertical, or diagonal row wins the game.";
+	var text = "Tic-tac-toe (or Noughts and crosses, Xs and Os) is a game for two players, X and O, who take turns marking the spaces in a 3x3 grid. The player who succeeds in placing three respective marks in a horizontal, vertical, or diagonal row wins the game.";
 	var hastick = false;
 
 	canvas.addEventListener("mousedown", function(evt) {
